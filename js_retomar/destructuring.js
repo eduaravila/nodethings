30d4cc8d8eb927b83a938e619431252afe997c64
Eduardo Avila (TUMATADOR) <jesus.amancilla@alumnos.udg.mx>
2019-01-01 23:07:12 -0600

    seccion 2 terminada

---
+++ js_retomar/destructuring.js
@@ -0,0 +1,5 @@
+let multiples = {nombre:"Eduardo",edad:20,objetivo:"Crecion de plataforma sobre adopcion de mascotas"}
+
+const {nombre:nuevoNombre,edad:nuevaEdad,objetivo:nuevoSueño} = multiples;
+
+console.log(nuevoNombre,nuevaEdad,nuevoSueño);
