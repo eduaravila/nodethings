271037d077842b0992de88632a18e74adebbaeaa
Eduardo Avila (TUMATADOR) <jesus.amancilla@alumnos.udg.mx>
2019-01-19 00:07:59 -0600

    fs para guardar info :metal:

--- MVC/models/productos.js
+++ MVC/models/productos.js
@@ -1,17 +1,44 @@
+const fs = require('fs')
+const path = require('path')
+
+const rootPath = require('../helpers/rootPath')
 
 class Producto {
-    constructor(){
-        this.productos = []
-    }
-    getProductos () {
-        return this.productos;
-    }
-    
-    agregarProducto(producto){
-        this.productos.push(producto)
-    }
+	constructor() {
+		fs.readFile(path.join(rootPath, 'data', 'productos.json'), (err, res) => {
+			if (err) {
+				this.productos = []
+				fs.mkdir(
+					path.join(rootPath, 'data'),
+					(error) =>
+						!error &&
+						fs.writeFile(
+							path.join(rootPath, 'data', 'productos.json'),
+							[],
+							(e, r) => !!e && console.log('erro', e)
+						)
+				)
+			} else {
+				this.productos = JSON.parse(res)
+			}
+		})
+	}
+	getProductos() {
+		return this.productos
+	}
+
+	agregarProducto(producto) {
+		this.productos.push(producto)
+		fs.writeFile(
+			path.join(rootPath, 'data', 'productos.json'),
+			JSON.stringify(this.productos),
+			(e, r) => {
+				console.log('error', e)
+			}
+		)
+	}
 }
 
 module.exports = {
-    Producto
+	Producto
 }
