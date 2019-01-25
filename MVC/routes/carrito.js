5075b55383f5fa3a7e5b2f0258ec05ed9d82dbf4
Eduardo Avila (TUMATADOR) <jesus.amancilla@alumnos.udg.mx>
2019-01-24 19:23:47 -0600

    destalles de producto :smile:

--- MVC/routes/carrito.js
+++ MVC/routes/carrito.js
@@ -7,8 +7,10 @@ const controller_compras = require('../controllers/admin/compras')
 routes.get('/carrito',(req,res,next)=> {
     res.render('carrito',{tituloPagina:'carrito',path:'/carrito',productos:['uno','dos','tres']})
 })
-routes.post('/realizarcompra',(req,res,next)=> {
-    res.render('carrito',{tituloPagina:'carrito',path:'/carrito',productos:['uno','dos','tres']})
+routes.post('/carrito',(req,res,next)=> {
+    console.log(req.body.id);
+    
+    res.redirect('/carrito')
 })
 
 
