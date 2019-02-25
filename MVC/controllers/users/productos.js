61c54429acb87a38a5c484743e32217d09a8210d
Eduardo Avila (TUMATADOR) <jesus.amancilla@alumnos.udg.mx>
2019-02-24 20:35:45 -0600

    paginacion agregado :panda_face:

--- MVC/controllers/users/productos.js
+++ MVC/controllers/users/productos.js
@@ -1,17 +1,34 @@
 const producto = require('../../models/productos').productos
+const MAX_PRODUCTOS = 3
+const getProductos = async (req, res, next) => {
+	try {
+		let  p= +req.query.p || 1
+		console.log(p,req.query);
+		
+		let productos = await producto
+			.find({})
+			.skip((p - 1) * MAX_PRODUCTOS)
+			.limit(MAX_PRODUCTOS)
+		let total_productos = await producto.find({}).countDocuments()
 
-const getProductos = async (req,res,next)=> {
-    try{
-        
-    let productos = await producto.find({});
-    res.render('productos',{productos,tituloPagina:"productos",activeUrl:"/productos", path:'/productos'})
-    }
-    catch(err){
-        console.log(err);
-        
-    }
+		res.render('productos', {
+			actual_elemento: p,
+			existe_siguiente: p * MAX_PRODUCTOS < total_productos,
+			existe_anterior: p > 1,
+			anterior_elemento: p - 1,
+            siguiente_elemento: p + 1,
+            primer_elemento:1,
+			ultimo_elemento: Math.ceil(total_productos / MAX_PRODUCTOS),
+			productos,
+			tituloPagina: 'productos',
+			activeUrl: '/productos',
+			path: '/productos'
+		})
+	} catch (err) {
+		console.log(err)
+	}
 }
 
-module.exports ={
-    getProductos
-};
+module.exports = {
+	getProductos
+}
